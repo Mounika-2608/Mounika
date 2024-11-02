58f7dd8260cc1626f62aef2d2925acb03f57876c
Mounika-2608 <mounikasingineni@gmail.com>
2024-11-02 22:24:39 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,4 @@
+// Contact button click event
+document.getElementById("contactButton").addEventListener("click", function() {
+    alert("Thank you for reaching out! We will contact you soon.");
+});
